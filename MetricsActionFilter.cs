2bf1c2be5385ed83da4f67eb4ecd862a842f065f
Taggrin <peter.neleman@hetnet.nl>
2024-08-18 11:47:09 +0200

    Some simple metrics

---
+++ MetricsActionFilter.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace WebAPIDemo
+{
+    public class MetricsActionFilter : IActionFilter
+    {
+        private readonly ILogger<MetricsActionFilter> logger;
+        private Stopwatch? stopwatch;
+
+        public MetricsActionFilter(ILogger<MetricsActionFilter> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            stopwatch = Stopwatch.StartNew();
+
+            logger.LogInformation("Starting request: {method} {url} at {time}",
+                context.HttpContext.Request.Method,
+                context.HttpContext.Request.Path,
+                DateTime.UtcNow);
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (stopwatch != null)
+                stopwatch.Stop();
+
+            logger.LogInformation("Finished request: {method} {url} at {time} and took {elapsed} ms.",
+                context.HttpContext.Request.Method,
+                context.HttpContext.Request.Path,
+                DateTime.UtcNow,
+                stopwatch?.ElapsedMilliseconds.ToString() ?? "unknown");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<MetricsActionFilter>();
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
