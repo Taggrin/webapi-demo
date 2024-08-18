24e60b232ac0280ffb9fbdbe9e7772ec7a92a3a4
Taggrin <peter.neleman@hetnet.nl>
2024-08-18 13:53:34 +0200

    Mark adding as required

--- WebAPIDemo/Models/AddProductRequest.cs
+++ WebAPIDemo/Models/AddProductRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace WebAPIDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIDemo.Models
 {
     public class AddProductRequest
     {
+        [Required]
         public string Description { get; set; } = string.Empty;
+        [Required]
         public decimal Price { get; set; }
     }
 }
