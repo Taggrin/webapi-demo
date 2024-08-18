727ba72952360bf6e80c9490d3493caca46652b1
Taggrin <peter.neleman@hetnet.nl>
2024-08-18 12:06:40 +0200

    Move project files to subdir and create test project

---
+++ WebAPIDemo.Tests/ProductControllerTests.cs
@@ -0,0 +1,16 @@
+using Moq;
+using WebAPIDemo.Services;
+
+namespace WebAPIDemo.Tests
+{
+    public class ProductControllerTests
+    {
+        private readonly Mock<IProductService> mockProductService;
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
